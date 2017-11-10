857f6afd807c2b65109dde41e7fa2f1c07507fff
Péter Sebők <sebok.peter97@gmail.com>
2017-11-10 23:02:56 +0100

    Starting out with Python

---
+++ PythonApplication1/PythonApplication1/HelloWorld.py
@@ -0,0 +1,8 @@
+
+#Standard "Hello World" program
+
+print('Hello World!')
+print('What is your name?')
+name = input()
+print('Nice to meet you ' + name)
+print('The length of your name is: ' + str(len(name)))
